abce11753c5b762bb4e0cc080294dd7a5802e9ce
Emmanuel <emmau25@gmail.com>
2017-10-16 01:06:04 -0500

    Update: Add called the API and show resultats

--- js/query.js
+++ js/query.js
@@ -8,19 +8,20 @@ $(document).ready(function(){
 			$("h2.name").text(json.name);
 		}
 	});
-
-
 	$.ajax({
 		url: 'https://swapi.co/api/people/?format=json',
 		type:'GET',
 		dataType: 'JSON',
 		success: function(per){
-			for (var i = 0; i < per.results.length; i++) {
+			for (var i = 0; i < 6; i++) {
+				var a = $("<div onclick=\"data2('"+per.results[i].films[0]+"')\" class='col-md-4'>" + per.results[i].name + "</div>");
+				$(".row").append(a);
 				console.log(per.results[i].name);
 			}
 		}
 	});
-
-
-
 });
+
+function data2(nom){
+	alert(nom);
+}
