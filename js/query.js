04f7f940e79fe56dfef2c0bea69b21391829d7bb
Emmanuel <emmau25@gmail.com>
2017-10-17 16:22:16 -0500

    Update: Add functions for see JSON features error corrections

--- js/query.js
+++ js/query.js
@@ -8,20 +8,97 @@ $(document).ready(function(){
 			$("h2.name").text(json.name);
 		}
 	});
-	$.ajax({
-		url: 'https://swapi.co/api/people/?format=json',
-		type:'GET',
-		dataType: 'JSON',
-		success: function(per){
-			for (var i = 0; i < 6; i++) {
-				var a = $("<div onclick=\"data2('"+per.results[i].films[0]+"')\" class='col-md-4'>" + per.results[i].name + "</div>");
-				$(".row").append(a);
-				console.log(per.results[i].name);
-			}
+
+	$(".row").append("<div class='col-md-12 text-center'><img src='images/load.gif' class='img-fluid'></img></p>");
+	$('.btnNext').hide();
+
+	const ITERACIONES_1=6;
+	const ITERACIONES_2=3;
+	let helper= {
+	  film:': ',
+	  counter:0,
+	  method: function (per,res) {
+		this.film += res.title+",";
+		this.counter++;
+		if (this.counter == 3) {
+		$(".row").empty();
+		for (var index = 0; index < ITERACIONES_1; index++) {
+			var a = $("<div data-toggle='modal' data-target='#myModal'" + 
+			"onclick=\"data2('"+per.results[index].name+"','"+per.results[index].height+
+			"','"+per.results[index].birth_year+"','"+per.results[index].mass+"','"+per.results[index].gender+"','"+this.film+"')\" class='col-md-4 text-center'><img src='images/st1.png' class='img-fluid'></img><p class=' text-center lead'>" +
+		  	per.results[index].name + "</p></div>");
+		$(".row").append(a);
+		  console.log(per.results[index].name);
+		  }
+		  $(".btnNext").attr("id", per.next);
+		  $(".btnNext").show();
 		}
-	});
+	  }
+	};
+	$.ajax({
+			url: 'https://swapi.co/api/people/?format=json',
+			type:'GET',
+			dataType: 'JSON',
+			crossDomain: true,
+			success: function(per){
+				var film;
+				for (var i = 0; i < ITERACIONES_1; i++) {
+					for (var index = 0; index < ITERACIONES_2; index++) {
+						var ul = per.results[i].films[index];
+						$.ajax({
+							url:ul,
+							type: 'GET',
+							dataType: 'JSON',
+							success:function(res){
+								helper.method(per,res);
+							}
+						}); 
+					} 
+				}
+			}
+		});
+
+		$(".btnNext").click(function(){
+		var res = $(".btnNext").attr("id")
+		console.log(res);
+			$.ajax({
+				url: res,
+				type:'GET',
+				dataType: 'JSON',
+				crossDomain: true,
+				success: function(per){
+					var film;
+					for (var i = 0; i < ITERACIONES_1; i++) {
+						for (var index = 0; index < ITERACIONES_2; index++) {
+							var ul = per.results[i].films[index];
+							$.ajax({
+								url:ul,
+								type: 'GET',
+								dataType: 'JSON',
+								success:function(res){
+									helper.method(per,res);
+								}
+							}); 
+						} 
+					}
+				}
+			});
+	
+		});
+
+		
+
+
 });
 
-function data2(nom){
-	alert(nom);
+function data2(nom,poid,anni,mass,genre,film){
+	//alert(nom);
+	$(".modal-body > div").remove();
+	$(".modal-title > p").remove();
+	$(".modal-title").append("<p>Description de "+nom+"</p>");
+	$(".modal-body").append("<div class='alert alert-primary'><p>Poid: "+poid+"</p></div>");
+	$(".modal-body").append("<div class='alert alert-secondary'><p>Anniversaire: "+anni+"</p></div>");
+	$(".modal-body").append("<div class='alert alert-success'><p>Masse: "+mass+"</p></div>");
+	$(".modal-body").append("<div class='alert alert-danger'><p>Genre: "+genre+"</p></div>");
+	$(".modal-body").append("<div class='alert alert-info'><p>Films Realisés"+film+"</p></div>");	
 }
